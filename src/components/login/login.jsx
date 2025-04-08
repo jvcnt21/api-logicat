d8ebc41f38b76b4726daa81b0278de47f9e5c466
júlio vicente <98538877+jvcnt21@users.noreply.github.com>
2025-04-07 23:54:26 -0300

    Add files via upload

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -14,10 +14,8 @@ export default function Login() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        // Adiciona a classe `login-page` ao <body> quando o componente é montado
         document.body.classList.add('login-page');
 
-        // Remove a classe `login-page` ao <body> quando o componente é desmontado
         return () => {
             document.body.classList.remove('login-page');
         };
@@ -25,17 +23,26 @@ export default function Login() {
 
     const handleLogin = () => {
         if (rememberMe) {
-            Cookies.set('username', username, { expires: 7 }); // Cookie expira em 7 dias
+            Cookies.set('username', username, { expires: 7 });
         } else {
             localStorage.setItem('username', username);
         }
-        navigate('/ListarParceiros'); // Redireciona para a página inicial
-        window.location.reload(); // Força o recarregamento da página para garantir que o estado do username seja atualizado
+        navigate('/ListarParceiros');
+        window.location.reload(); 
     };
 
     return (
         <div className="login-page" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <Card title="Login" style={{ width: '300px' }}>
+            <Card 
+            style={{ width: '300px' }} 
+            header={
+                <div className="login-header">
+                    <img src="src/assets/icon.png" alt="Login Icon" className="login-icon" />
+                    <span className="login-title">Login</span>
+                </div>
+            }
+            >
+
                 <div className="p-fluid">
                     <div className="field">
                         <label htmlFor="username">Usuário</label>
@@ -45,13 +52,11 @@ export default function Login() {
                         <label htmlFor="password">Senha</label>
                         <InputText id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                     </div>
-                    <div className="field-checkbox">
-                        <Checkbox inputId="rememberMe" checked={rememberMe} onChange={(e) => setRememberMe(e.checked)} />
-                        <label htmlFor="rememberMe">Manter Conectado</label>
-                    </div>
                     <Button label="Entrar" onClick={handleLogin} className="btn-entrar p-mt-3" />
                 </div>
             </Card>
         </div>
     );
+
+    
 }
