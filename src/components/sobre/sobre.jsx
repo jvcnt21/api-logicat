48d484a19e89cc9540450f00659d71e509174737
júlio vicente <98538877+jvcnt21@users.noreply.github.com>
2025-04-08 08:04:40 -0300

    Update sobre.jsx

--- src/components/sobre/sobre.jsx
+++ src/components/sobre/sobre.jsx
@@ -9,19 +9,10 @@ export default function Sobre() {
     <div className="sobre flex flex-column justify-content-between min-h-screen w-full p-4">
       <div className="conteudo flex flex-column gap-5 max-w-screen-lg mx-auto">
           <section>
-          <h2 className="text-xl font-semibold mb-2">Descrição</h2>
-            <p>
-            Este projeto é uma aplicação de micro front-ends (facilitando o desenvolvimento individiual de um módulo especifico)
-            para cadastro e gestão de parceiros e empresas externas. 
-            </p>
-            <p>
-            A aplicação utiliza React (com PrimeReact) e Angular 15.
-            O projeto é containerizado para facilitar o deploy em ambientes de cloud, como AWS ECS.
-            </p>
 
           <h2 className="text-xl font-semibold mb-2">Tecnologias Utilizadas</h2>
             <ul className="list-disc pl-4">
-              <li>Frontend: React, PrimeReact, Angular 15</li>
+              <li>Frontend: React, PrimeReact</li>
               <li>Containerização: Docker</li>
               <li>JS-Cookie: Gestão de cookies</li>
               <li>Vite: Build da aplicação</li>
